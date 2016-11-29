97dbe9fc976281cf5e4ba8f821c80862fd88ea73
zhangwenzw <604639246@qq.com>
2016-11-30 00:16:30 +0800

    add

---
+++ js/baidu.js
@@ -0,0 +1,30 @@
+$(function(){
+    var ch=$(window).height();
+    var num=0;
+    var flag=true;
+    touch.on("body","swipeup","#fullpage",function(){
+        if(!flag){
+            return;
+        }
+        num++;
+        if(num==$("#fullpage>section").length){
+            num=$("#fullpage>section").length-1;
+        }
+        $("#fullpage").css({marginTop:-num*ch,transition:"margin 1s ease"});
+        flag=false;
+    })
+    touch.on("body","swipedown","#fullpage",function(){
+        if(!flag){
+            return;
+        }
+        num--;
+        if(num==-1){
+            num=0;
+        }
+        $("#fullpage").css({marginTop:-num*ch,transition:"margin 1s ease"});
+        flag=false;
+    })
+    $("#fullpage")[0].addEventListener("webkitTransitionEnd",function(){
+        flag=true;
+    })
+})
