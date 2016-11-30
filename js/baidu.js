81239f50b0a42cbfd259484a841c207f645687e3
zhangwenzw <604639246@qq.com>
2016-12-01 00:20:29 +0800

    add

--- js/baidu.js
+++ js/baidu.js
@@ -1,4 +1,8 @@
 $(function(){
+    var ch=$(window).height();
+    var cw=$(window).width();
+    var num=0;
+    var flag=true;
     //导航
     var flag2=true;
     $(".menu-option").click(function(){
@@ -48,9 +52,7 @@ $(function(){
     $("#fullpage").mouseover(function(e){
         e.preventDefault();
     })
-    var ch=$(window).height();
-    var num=0;
-    var flag=true;
+
     touch.on("body","swipeup","#fullpage",function(){
         if(!flag){
             return;
@@ -60,20 +62,6 @@ $(function(){
             num=$("#fullpage>section").length-1;
         }
         $("#fullpage").css({marginTop:-num*ch,transition:"margin 1s ease"});
-        $("section").each(function(i,obj){
-            if(i==0){
-                return;
-            }
-            if(i==num){
-                $(obj).find(".sleft-con").css({opacity:1,transform:"translate(0,0)",transition:"transform 1s ease"});
-                $(obj).find(".sright-con").css({opacity:1,transform:"translate(0,0)",transition:"transform 1s ease"});
-
-            }else{
-
-                $(obj).find(".sleft-con").css({opacity:0,transform:"translate(-50px,0)"});
-                $(obj).find(".sright-con").css({opacity:0,transform:"translate(50px,0)"});
-            }
-        })
         flag=false;
     })
     touch.on("body","swipedown","#fullpage",function(){
@@ -84,32 +72,58 @@ $(function(){
         if(num==-1){
             num=0;
         }
-        $("section").each(function(i,obj){
-            if(i==0){
-                return;
-            }
-            if(i==num){
-                $(obj).find(".sleft-con").css({opacity:1,transform:"translate(0,0)",transition:"transform 1s ease"});
-                $(obj).find(".sright-con").css({opacity:1,transform:"translate(0,0)",transition:"transform 1s ease"});
-
-            }else{
-
-                $(obj).find(".sleft-con").css({opacity:0,transform:"translate(-50px,0)"});
-                $(obj).find(".sright-con").css({opacity:0,transform:"translate(50px,0)"});
-            }
-        })
+        if(num==0){
+            $("section").not($("section").eq(num)).find(".sleft-con").css({
+                opacity:0,transform:"translateX(-50px)"
+            })
+            $("section").not($("section").eq(num)).find(".sright-con").css({
+                opacity:0,transform:"translateX(50px)"
+            })
+        }
+        //$("section").each(function(i,obj){
+        //    if(i==0){
+        //        return;
+        //    }
+        //    if(i==num){
+        //        $(obj).find(".sleft-con").css({opacity:1,transform:"translate(0,0)",transition:"transform 1s ease"});
+        //        $(obj).find(".sright-con").css({opacity:1,transform:"translate(0,0)",transition:"transform 1s ease"});
+        //
+        //    }else{
+        //
+        //        $(obj).find(".sleft-con").css({opacity:0,transform:"translate(-50px,0)"});
+        //        $(obj).find(".sright-con").css({opacity:0,transform:"translate(50px,0)"});
+        //    }
+        //})
         $("#fullpage").css({marginTop:-num*ch,transition:"margin 1s ease"});
         flag=false;
     })
+
     $("#fullpage")[0].addEventListener("webkitTransitionEnd",function(){
         flag=true;
-    })
+        $("section").not($("section").eq(num)).find(".sleft-con").css({
+            opacity:0,transform:"translateX(-50px)"
+        })
+        $("section").not($("section").eq(num)).find(".sright-con").css({
+            opacity:0,transform:"translateX(50px)"
+        })
+        if(num==0){
+            return;
+        }else{
+            $("section").eq(num).find(".sleft-con").css({
+                opacity:1,transform:"translateX(0px)"
+            })
+            $("section").eq(num).find(".sright-con").css({
+                opacity:1,transform:"translateX(0px)"
+            })
+        }
+    },false)
 
     //监测浏览器宽度
     $(window).resize(function(){
-        var ch=$(window).height();
+        ch=$(window).height();
+        $("#fullpage>section").height(ch);
         $("#fullpage").css({marginTop:-num*ch,transition:"margin 1s ease"});
-        var cw=$(window).width();
+        cw=$(window).width();
         if(cw>1000){
             $(".menu a").css({
                 animation:"none",
